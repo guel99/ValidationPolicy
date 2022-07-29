bf5a9f5c2ece27c9672da09173a09538df8e30cc
guel99 <migueloliveira985@gmail.com>
2022-07-29 20:00:54 +0100

    Implementation of the following builder: SignaturePolicyType --> AcceptableContainterTypes

--- src/main/java/com/devisefutures/validationpolicies/mappers/PoliciesMapper.java
+++ src/main/java/com/devisefutures/validationpolicies/mappers/PoliciesMapper.java
@@ -1,8 +1,8 @@
 package com.devisefutures.validationpolicies.mappers;
 
-import com.devisefutures.validationpolicies.models.gen.ConstraintsParameters;
-import com.devisefutures.validationpolicies.models.gen.ContainerConstraints;
-import com.devisefutures.validationpolicies.models.gen.SignaturePolicyType;
+import com.devisefutures.validationpolicies.models.gen.*;
+
+import java.util.List;
 
 /**
  * Class that implements methods to build a ValidationPolicy
@@ -16,8 +16,34 @@ public class PoliciesMapper {
         return dssPolicy;
     }
 
+    /**
+     * Builds the containerConstraints of DSS ValidationPolicy by passing a
+     * SignaturePolicyType (ETSI Signature Policy)
+     * @param signaturePolicy
+     * @return
+     */
     private ContainerConstraints buildContainerConstraints(SignaturePolicyType signaturePolicy) {
+        MultiValuesConstraint acceptableContainerTypes = this.getAcceptableContainerTypes(signaturePolicy);
         ContainerConstraints containerConstraints = new ContainerConstraints();
+        containerConstraints.setAcceptableContainerTypes(acceptableContainerTypes);
         return containerConstraints;
+    }
+
+    /**
+     * Creates a list of all AcceptableContainerTypes accepted by the validation policy
+     * @param signaturePolicy The validation policy applied
+     * @return MultiValuesConstraint all acceptableContainerTypes
+     */
+    private MultiValuesConstraint getAcceptableContainerTypes(SignaturePolicyType signaturePolicy){
+        MultiValuesConstraint acceptableContainerTypes = new MultiValuesConstraint();
+        for(PolicyRuleType policyRule : signaturePolicy.getPolicyComponents().getPolicyRules().getPolicyRule()){
+            List<RelativePositionValuesType> sigDTBSRelativePosition = policyRule.getSigToDTBSRelationRules().getSigDTBSRelativePosition();
+            if(sigDTBSRelativePosition != null && policyRule.getRecommendedScope().contains(ScopeType.VALIDATION)
+                    && sigDTBSRelativePosition.contains(RelativePositionValuesType.A_SI_C)) {
+                acceptableContainerTypes.getId().add("ASIC-S");
+                acceptableContainerTypes.getId().add("ASIC-E");
+            }
+        }
+        return acceptableContainerTypes;
     }
 }
